fed164c40c80763f72554cd3292d738197e38100
Roman Mager <roman.mager@outlook.com>
2020-05-01 13:06:31 +0300

    Add project table and sample data in sql scripts

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -12,3 +12,28 @@ VALUES ('Task #2');
 
 INSERT INTO Tasks(task_name)
 VALUES ('Task #3');
+
+--
+CREATE TABLE IF NOT EXISTS project
+(
+    project_id   BIGINT      NOT NULL AUTO_INCREMENT,
+    description  VARCHAR(400),
+    project_name VARCHAR(50) NOT NULL,
+    PRIMARY KEY (project_id)
+);
+
+
+INSERT INTO project(project_name, description)
+VALUES ('Demo project #1', 'Demo description #1');
+
+INSERT INTO project(project_name, description)
+VALUES ('Demo project #2', 'Demo description #2');
+
+INSERT INTO project(project_name, description)
+VALUES ('Demo project #3', 'Demo description #3');
+
+INSERT INTO project(project_name, description)
+VALUES ('Sample project #1', 'Sample description #1');
+
+INSERT INTO project(project_name, description)
+VALUES ('Sample project #2', 'Sample description #2');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,16 @@
 DROP TABLE IF EXISTS Tasks;
+DROP TABLE IF EXISTS project;
 
 CREATE TABLE IF NOT EXISTS Tasks
 (
     task_id   INT          NOT NULL PRIMARY KEY AUTO_INCREMENT,
     task_name VARCHAR(300) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS project
+(
+    project_id   BIGINT      NOT NULL AUTO_INCREMENT,
+    description  VARCHAR(400),
+    project_name VARCHAR(50) NOT NULL,
+    PRIMARY KEY (project_id)
 );
